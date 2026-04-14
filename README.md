92655e188e014bed40af9f431f4762fb7cc7ed2d
ananyasayani3 <sayani.ananya2@gmail.com>
2026-04-14 11:10:51 -0700

    importing each file

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Schedupaper
-ScheduPaper is a web application designed to help students pinpoint important time-marks within the writing process. The application takes in information about the essay, and its final deadline, and recommend a timeline based on the user's work preference.
+# ScheduPaper
+
+Latest *important* update made on: May 10th, 2023
+
+## Overview 
+<p> ScheduPaper is a web application designed to help students pinpoint important time-marks within the writing process. The application takes in information about the essay, and its final deadline, and recommend a timeline based on the user's work preference. </p>
+<p>The goal of the application is to help students visualize the approximate time needed to complete their essays, as well as</p>
+<p><em>Visit ScheduPaper at:</em> https://cyborf.github.io/ScheduPaper/</p>
+
+## Current Functionalities
+
+The user is able to input:
+<ul>
+    <li> Name of essay
+    <li> Remaining word count
+    <li> Remaining dates
+    <li> Hours each day for writing
+</ul></p><p>
+Output:
+<ul>
+    <li> Schedule made by Greedy Algorithm
+</ul>
+
+## Goals for Future Update
+<ul>
+    <li> Schedule made with a more realistic algorithm
+    <li> Schedule for multiple essays, each with customizable sections!
+    <li> Smarter user-interface (more interactive)
+</ul>
+
+## Group Members
+<p> This web application is our final project for Oberlin College's CS241 Systems Programming course. Version update on May 12 was a collaborative effort between:
+<ul>
+    <li> Dan-Ha Le '25 (@cyborf)
+    <li> Ananya Sayani '25 (@ananyasayani)
+    <li> Zaina Saleem '25 (@zainasaleem)
+    <li> Miriam Rairick '25 (@mjrairick)
+
+</ul>
+<p> For the summer of 2023, we will be working on adding more functionalities for the web-tool. Our team will consist of:<p>
+    <ul>
+        <li> Dan-Ha Le '25 (@cyborf)
+        <li> Abby Lewin '25
+</ul>
+</p>
---
+++ script.js
@@ -0,0 +1,99 @@
+var mod = false;
+
+function GreedyAlgorithm(essay, schedule){
+    document.getElementById('error').innerHTML = "";
+    // essay is the [1, 3, 1] ratio, schedule is [1, 1, 1,...]
+
+    // The following code assigns each index of assign to an array that shows how many hours to each part:
+
+    
+    let data = new Array();
+    let pointer = 0;
+    for (let i=0; i<schedule.length; i++) {
+        let division = [0, 0, 0];
+        if (pointer >=3) break;
+        while (schedule[i]> 0) {
+            // Int
+            if (schedule[i] >= essay[pointer]) {
+                let assign = essay[pointer];
+                schedule[i] = schedule[i] - essay[pointer];
+                division[pointer] = assign;
+                pointer=pointer+1;
+            }
+            else {
+                let assign = schedule[i];
+                division[pointer] = assign;
+                essay[pointer] = essay[pointer] - assign;
+                schedule[i] = 0;
+            }
+        }
+        data.push(division);
+
+    }
+
+    // Prints the HTML list:
+    let myResults = document.getElementById('results');
+    let counter = 1;
+    data.forEach((item)=>{
+        let li = document.createElement("li");
+        li.innerText = 
+        `Day ${counter}: 
+                  Introduction (${item[0]} hour)
+                  Body (${item[1]} hour)
+                  Conclusion (${item[2]} hour) `;
+        // li.innerText = "Day".concat(" ", counter);
+        counter = counter + 1;
+        myResults.appendChild(li);
+       
+    })
+    document.getElementById('resultsHeader').innerHTML = "The calendar we've generated for you is..."
+
+
+
+
+}
+
+function clean() {
+    // resetting the messages on the html page 
+    document.getElementById('error').innerHTML=" ";
+    document.getElementById('results').innerHTML=" ";
+    document.getElementById('resultsHeader').innerHTML=" ";
+
+}
+
+function makeSchedule() {
+    console.log(
+        'hi'
+    )
+    clean();
+    let name = document.querySelector("#paperName");
+    let length = document.querySelector("#paperLength");
+    let days = document.querySelector("#paperDays");
+    let dailyHours = document.querySelector("#paperDailyHours");
+    let pace = document.querySelector("#paperPace");
+
+    essayHours = length.value/pace.value;
+    totalHours = days.value*dailyHours.value;
+    if (!length.value > 0) return document.getElementById('error').innerHTML="Please input the essay's word count";
+    if (!days.value > 0) return document.getElementById('error').innerHTML="Please give the essay a duration";
+    if (!dailyHours.value > 0) return document.getElementById('error').innerHTML="Please give a daily limit";
+    if (!pace.value > 0) return document.getElementById('error').innerHTML="Please let us know your writing speed!";
+    if (essayHours > totalHours) return document.getElementById('error').innerHTML="This project is not feasible.";
+
+    // Setting up the essay:
+    myEssay =  [1, 3, 1];
+    for (let i = 0; i<3; i++) {
+        myEssay[i] = essayHours/5*myEssay[i];
+    }
+
+    // Setting up the schedule:
+    mySchedule = new Array(days.value);
+    for (let i = 0; i<days.value; i++) {
+        mySchedule[i] = dailyHours.value;
+
+    }
+
+    GreedyAlgorithm(myEssay, mySchedule);
+
+
+}
